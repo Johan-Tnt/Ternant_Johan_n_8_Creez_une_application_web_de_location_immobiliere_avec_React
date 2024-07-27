f0b7f4b2f57c68fb3f1b6ec9bc1a4c1c2f0b50ec
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2024-07-27 11:52:14 +0200

    Creation of project files + home and about pages + Sass and React Router initialized.

---
+++ latest/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import LogoKasa from "../assets/img/LogoKasa.png";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={LogoKasa} alt="Logo Kyra" />
+      <nav>
+        <ul>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "underline" : "")}
+          >
+            <li>Acceuil</li>
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "underline" : "")}
+          >
+            <li>A propos</li>
+          </NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ latest/src/components/pages/About.jsx
@@ -0,0 +1,11 @@
+//import ImgAbout from "../assets/img/ImgAbout.png";
+//<img src={ImgAbout} alt="Img About" />;
+const About = () => {
+  return (
+    <div>
+      <h1>A Propos</h1>
+    </div>
+  );
+};
+
+export default About;
